26d25a50e70ba692bc4d3a08980e5eee654b81ff
Daniel Polukchi <danilabodrov746@gmail.com>
2024-08-25 07:52:25 +0000

    Save progress: 950.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -4,9 +4,12 @@
 
 echo -e "\n~~ Bingo Number Generator ~~\n"
 
-NUMBER=$(( $RANDOM % 75+1 ))
+NUMBER=$(( $RANDOM % 15+1 ))
 TEXT="The next number is, "
 
-echo $NUMBER
+if (( NUMBER <= 15 )) 
+then
+  echo $TEXT B:$NUMBER 
+fi
 
-echo $TEXT
+
